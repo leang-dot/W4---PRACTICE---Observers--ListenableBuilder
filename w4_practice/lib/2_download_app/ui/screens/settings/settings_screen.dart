b6a015973cc7ad2b32ad5f2e391a17931b21e398
leang-dot <ounleang180@gmail.com>
2026-02-26 13:56:11 +0700

    TASK2-DOWNLOADER-COLOR-THEME

--- w4_practice/lib/2_download_app/ui/screens/settings/settings_screen.dart
+++ w4_practice/lib/2_download_app/ui/screens/settings/settings_screen.dart
@@ -10,42 +10,52 @@ class SettingsScreen extends StatelessWidget {
  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: currentThemeColor.backgroundColor,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 16),
-          Text(
-            "Settings",
-            style: AppTextStyles.heading.copyWith(
-              color: currentThemeColor.color,
-            ),
+    ValueNotifier<ThemeColor> colorTheme = ValueNotifier<ThemeColor>(
+      currentThemeColor,
+    );
+    return ValueListenableBuilder(
+      valueListenable: colorTheme,
+      builder: (context, value, child) {
+        return Container(
+          color: colorTheme.value.backgroundColor,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 16),
+              Text(
+                "Settings",
+                style: AppTextStyles.heading.copyWith(
+                  color: currentThemeColor.color,
+                ),
+              ),
+        
+              SizedBox(height: 50),
+        
+              Text(
+                "Theme",
+                style: AppTextStyles.label.copyWith(color: AppColors.textLight),
+              ),
+        
+              SizedBox(height: 10),
+        
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: ThemeColor.values
+                    .map(
+                      (theme) => ThemeColorButton(
+                        themeColor: theme,
+                        isSelected: theme == value,
+                        onTap: (value) {
+                          colorTheme.value = theme;
+                         },
+                      ),
+                    )
+                    .toList(),
+              ),
+            ],
           ),
-
-          SizedBox(height: 50),
-
-          Text(
-            "Theme",
-            style: AppTextStyles.label.copyWith(color: AppColors.textLight),
-          ),
-
-          SizedBox(height: 10),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: ThemeColor.values
-                .map(
-                  (theme) => ThemeColorButton(
-                    themeColor: theme,
-                    isSelected: theme == currentThemeColor,
-                    onTap: (value) { },
-                  ),
-                )
-                .toList(),
-          ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
